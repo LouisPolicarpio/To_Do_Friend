73c4a4fb5cc204d6bc31b5fb3cfa2d9f52473129
LouisPolicarpio <louis.policarpio@gmail.com>
2025-04-04 14:38:33 +1100

    implemnted: front end visulisation of score decrease (this is just an aproximation and does not directly reflect backend or impact score on backend)

--- frontend/src/components/DisplayPoints.jsx
+++ frontend/src/components/DisplayPoints.jsx
@@ -1,9 +1,28 @@
 
 import React from 'react'
 import { useGetPoints } from '../hooks/usePoints'
+import { useEffect,useState} from 'react';
+
 
 function DisplayPoints() {
   const { data, isLoading, isError,error } = useGetPoints(); 
+  const UpdatePeriod = 60000 //1min in ms 
+  const [score, setScore] = useState(0)
+  const scoreDecrease = 100
+
+  useEffect(()=>{
+    if(data){
+        setScore(data.score);
+    }
+  },[data]);
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setScore((prevScore) => prevScore - scoreDecrease);
+    }, UpdatePeriod);
+
+    return () =>  clearInterval(interval)
+  }), [scoreDecrease];
 
     // Handle loading and error states
   if (isLoading) return <div>Loading...</div>;
@@ -12,7 +31,7 @@ function DisplayPoints() {
   return (
     <div className='items-center text-center'>
         <h1>score</h1>
-        <h1> {data?.score}</h1>
+        <h1> {score}</h1>
     </div>
   )
 }
